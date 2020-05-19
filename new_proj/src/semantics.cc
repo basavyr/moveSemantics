76dd7f7c8c02d71be90a774df749b1e093de9936
Robert Poenaru <robert.poenaru@protonmail.ch>
2020-05-19 19:34:54 +0300

    started actual rvalues-lvalues test source

--- new_proj/src/semantics.cc
+++ new_proj/src/semantics.cc
@@ -14,8 +14,28 @@ semantics::Semantics::~Semantics()
     line();
 }
 
+int returnValue()
+{
+    return 5;
+}
+
 int main()
 {
-    semantics::Semantics x = semantics::Semantics();
-    // x.~Semantics();
+    // semantics::Semantics x = semantics::Semantics();
+    int x; //lvalue
+    x = 1;
+    const int cx = 1;
+    // int&y=returnValue(); //doesn't work since y is an l-value reference and it only binds to lvalues
+
+    const int &ry = returnValue(); //it works since const references can bind to both lvalues and rvalues
+    //it extends the life-time of @returnValue until the next scope.
+    //! no modification on this rvalue can be further executed
+
+    //rvalue references
+    int &&rvalue = returnValue(); //this only binds to r-values
+    //  int &&new_rvalue = cx; ! Doesn't work since rvalue references only bind to rvalues
+
+    rvalue = ry;
+    std::cout << rvalue;
+    std::cout << std::endl;
 }
---
+++ scripts/run.sh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+#type -a cmd 
+# the above command finds aliases
+ /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang++ -std=c++14 ../new_proj/src/semantics.cc
+./a.out
