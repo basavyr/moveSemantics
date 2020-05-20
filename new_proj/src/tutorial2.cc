8635db82c6f4e02e7eb90d54df31c568f8f65c90
Robert Poenaru <robert.poenaru@protonmail.ch>
2020-05-20 13:05:14 +0300

    started second tutorial on move senatics. link in source

---
+++ new_proj/src/tutorial2.cc
@@ -0,0 +1,13 @@
+#include <vector>
+
+#include <iostream>
+
+inline void line()
+{
+    std::cout << '\n';
+}
+
+int main()
+{
+    line();
+}
---
+++ scripts/move2.sh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+#type -a cmd
+# the above command finds aliases
+/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang++ -std=c++14 ../new_proj/src/tutorial2.cc -o move2.out
+./move2.out
